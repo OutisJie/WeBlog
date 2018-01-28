423da06d798f6f7022cdcd667f34dcc59aa6eca5
OutisJie <1014547032@qq.com>
2018-01-28 16:43:07 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My First Angular App';
+  blogger: Blogger = {
+    id: 1,
+    name: 'OutisJie'
+  };
+}
+export class Blogger {
+  id: number;
+  name: string;
 }
